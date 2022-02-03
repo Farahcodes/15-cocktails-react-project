3907768ce0e72afec9918acb53f73cd05265a563
farah <farah.fournat@gmail.com>
2022-02-03 20:36:37 +0100

    setting up error and about page

--- setup/src/pages/About.js
+++ setup/src/pages/About.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error, necessitatibus? Eos excepturi ipsam animi odio, quasi dicta similique facilis nihil voluptatibus omnis veritatis odit architecto vero maxime? Debitis cumque explicabo, ex delectus consequuntur error accusantium vitae est suscipit iusto ipsa.</p>
+    </section>
   )
 }
 
